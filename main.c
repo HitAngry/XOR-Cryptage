0745f25bad8d8e4a11c3e08799efda52b5d77b36
Simon Philouze <hiteangry@gmail.com>
2014-06-30 15:18:14 +0200

    Main 0.4

--- main.c
+++ main.c
@@ -1,10 +1,67 @@
 #include <stdlib.h>
 #include <stdio.h>
+#include <string.h>
+
+#define LONG_MAX 5555
+
+void xor_file (FILE* source, FILE* destination, char* key);
+void error(FILE *file, char *filename);
+int xor(char *src, char *dest, char *key);
 
 int main(int argc, char *argv[])
 {
-  printf("bonjour");
+  long double e = LONG_MAX;
 
-  printf("\rtest");
+  if(argc > 3)
+  {
+    xor (argv[1], argv[2], argv[3]);   
+  }
+
+  return EXIT_SUCCESS;
+}
+
+int xor(char *src, char *dest, char *key)
+{
+  FILE *source, *destination;
+
+  if(((source = fopen (src,"rb")) != NULL) && ((destination = fopen (dest,"wb")) != NULL))
+  {
+      xor_file(source, destination, key);
+
+      fclose (source);
+
+      fclose (destination);
+
+      return 1;
+  }
+
   return 0;
+}
+
+void xor_file (FILE *source, FILE *destination, char *key)
+{
+  long taille,h;
+  int i,c;
+
+  i = 0;
+  
+  taille = strlen (key);
+  
+  h = fgetc (source);
+
+  while (!feof (source))
+  {
+      c = h ^ key [i];
+      
+      fputc (c, destination);
+      
+      i++;
+      
+      if(i == taille) 
+      {
+	i = 0;
+      }
+      
+      h = fgetc (source);
+  }
 }
